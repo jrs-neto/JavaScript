4b8b39b2d60e72ad60fa4ed6d46c92f6024f0a4e
José Rodrigues <jrd.santosneto@gmail.com>
2023-11-30 13:57:27 -0300

    Tipos de Dados

---
+++ tipos-de-dados/script-aula.js
@@ -0,0 +1,26 @@
+let idade = 28;
+let simbolo = Symbol();
+console.log(simbolo);
+
+let nome = 'José';
+let lastName = 'Rodrigues';
+let fullName = nome + ' ' + lastName;
+
+console.log(fullName);
+
+// let gols = 1000;
+// let frase = 'Romário fez ' + gols + ' gols';
+// console.log(typeof frase);
+
+let ano = '2018';
+let mes = 8;
+console.log(ano + mes);
+
+let melhor = 'teste';
+let frase1 = "Esse é o \"melhor\" jogo";
+console.log(frase1);
+
+let gols = 1000;
+let frase2 = `Romário fez ${gols * 2} gols`;
+
+console.log(frase2);
---
+++ tipos-de-dados/script.js
@@ -0,0 +1,19 @@
+// Declare uma variável contendo uma string
+  let oneString = 'José Rodrigues'
+// Declare uma variável contendo um número dentro de uma string
+  let numberInString = '30';
+// Declare uma variável com a sua idade
+  let myAge = 30;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+  let name = 'José';
+  let lastName = 'Rodrigues';
+  let fullName = `${name} ${lastName}`;
+  console.log(fullName);
+
+// Coloque a seguinte frase em uma variável: It's time
+  let frase = "It's time";
+  console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof name);
