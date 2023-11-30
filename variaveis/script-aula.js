600c52e5cf028b5b479aba98aac6d2d8747463d4
José Rodrigues <jrd.santosneto@gmail.com>
2023-11-30 13:54:53 -0300

    variaveis

---
+++ variaveis/script-aula.js
@@ -0,0 +1,26 @@
+let nome = 'José';
+let idade = 30;
+let possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+let preco = 25;
+let totalComprado = 5;
+let totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+let sobrenome = 'Rodrigues', 
+    cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+let semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoFoiDefinida);
+
+let time = 'Vasco';
+time = 'Flamengo';
+
+console.log(time);
---
+++ variaveis/script.js
@@ -0,0 +1,21 @@
+// Declarar uma variável com o seu nome
+let name = 'José';
+
+// Declarar uma variável com a sua idade
+let age = 30;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let favoriteFood;
+
+// Atribuir valor a sua comida favorita
+favoriteFood = 'Cake';
+
+// Declarar 5 variáveis diferentes sem valores
+let lastName, 
+    city, 
+    college, 
+    adress, 
+    bestFriend;
+
+console.log(name, age, favoriteFood);
