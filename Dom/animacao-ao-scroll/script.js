40dd229e7ef52a1660e2144594e6bae2964fff46
jrs-neto <jrd.santosneto@gmail.com>
2024-01-22 12:38:05 -0300

    Added Subject and Exercise

--- Dom/animacao-ao-scroll/script.js
+++ Dom/animacao-ao-scroll/script.js
@@ -65,3 +65,25 @@ function initScrollSuave() {
   });
 }
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  const windowMetade = window.innerHeight * 0.6;
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if (isSectionVisible)
+          section.classList.add('ativo');
+        else
+          section.classList.remove('ativo');
+      })
+    }
+  }
+
+  animaScroll()
+
+  window.addEventListener('scroll', animaScroll);
+}
+initAnimacaoScroll();
