2818bfd39bf80ac1676dfea0cdf23d50af2b2dee
jrs-neto <jrd.santosneto@gmail.com>
2024-02-26 13:05:23 -0300

    Added Subject

---
+++ automacao-front-end/npm/script.js
@@ -0,0 +1,18 @@
+// NPM (NODE PACKAGE MANAGER)
+// Linha de comando - $ npm install lodash https://www.npmjs.com/
+
+// INSTALAR PACOTES
+// $ npm install lodash - Instala o pacote lodash no diretório atual da linha de comando
+
+// $ npm install eslint -g - Instala o eslint globalmente (No mac é necessário o sudo para instalar globalmente.)
+
+// $ npm update lodash - Atualiza o pacote lodash
+
+// $ npm uninstall lodash - Desinstala o pacote lodash
+
+// PACKAGE.JSON
+// package.json - Arquivo local com as configurações e dependências de pacotes NPM
+
+// $ npm init - Inicia uma nova configuração local do npm.
+
+// $ npm install - Instala todas as dependências listadas no arquivo package.json
